73a30403a3ba252464ea28cba3a4bba45c7dae2b
Cristian Trapero <critramo@gmail.com>
2022-09-04 23:40:56 +0200

    scan nodes working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WifiWizard2 } from '@awesome-cordova-plugins/wifi-wizard-2/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, WifiWizard2],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var WifiWizard2: any;
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
 
-  constructor() {}
+export class Tab1Page implements OnInit {
 
+  public currentNetwork: string;
+  public message: string;
+  public error: string;
+  public nodes = [];
+
+  constructor() {
+    this.getNetworks();
+  }
+
+  async getNetworks() {
+    WifiWizard2.getConnectedSSID().then(ssid => { this.currentNetwork = ssid; });
+
+    this.message = 'Searching for nodes...';
+
+    try {
+      this.nodes = [];
+      let wifis = await WifiWizard2.scan();
+      this.nodes = wifis.filter(wifi => wifi.SSID.startsWith('NODE-'));
+      this.message = '';
+      if(this.nodes.length === 0) {
+        this.message = 'No nodes found. Please try again.';
+      }
+    } catch (error) {
+      if (error === 'WIFI_NOT_ENABLED') {
+        this.message = 'Please enable WiFi to scan for nodes';
+      } else {
+        this.error = error;
+      }
+    }
+  }
+
+  async connectToNode(node) {
+
+    try {
+      this.message = 'Connecting to ' + node.SSID + '...';
+      let config = await WifiWizard2.formatWifiConfig(node.SSID, false);
+      await WifiWizard2.add(config);
+      await WifiWizard2.enable(node.SSID, true, true);
+    } catch (error) {
+      this.error = error;
+    }
+  }
+
+  ngOnInit(): void {
+  }
 }
