3415a1510d060e8f37f24d532e0ee30f2c5084e2
Cristian Trapero <critramo@gmail.com>
2022-09-04 18:37:22 +0200

    add ios support

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.cristiantrapero.molora',
   appName: 'molora',
   webDir: 'www',
   bundledWebRuntime: false
