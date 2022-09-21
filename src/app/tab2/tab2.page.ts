3b695ad44010ad6caa27cb632237908293c810ad
Cristian Trapero <critramo@gmail.com>
2022-09-21 22:48:14 +0200

    get info working

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,23 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NodeService } from '../services/node.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page implements OnInit{
+export class Tab2Page{
 
-  constructor(private nodeService: NodeService) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.nodeService.getInfo().subscribe((data) => {
-      console.log(data);
-    });
-
-    this.nodeService.getNodes().subscribe((data) => {
-      console.log(data);
-    });
-
-  }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NodeService } from '../services/node.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  public info: Observable<any>;
+  public nodes: Observable<any>;
+  public error: string;
+
+  constructor(private nodeService: NodeService) {}
+
+  ngOnInit() {
+    this.getInfo();
+    this.getNodes();
+  }
+
+  ionViewWillEnter() {
+    this.getInfo();
+    this.getNodes();
+  }
+
+  getInfo(){
+    this.nodeService.getInfo().subscribe((data) => {
+      this.info = data;
+      console.log(this.info);
+    }, (error) => {
+      console.log(error);
+    });
+  }
+
+  getNodes(){
+    this.nodeService.getNodes().subscribe((data) => {
+      this.nodes = data;
+      console.log(this.nodes);
+    }, (error) => {
+      console.log(error);
+    });
+  }
 
 }
